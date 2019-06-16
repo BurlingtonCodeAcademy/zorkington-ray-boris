501d07c440e4d7f011787f8765fb07991a3a7a1d
bkzboris <50851910+bkzboris@users.noreply.github.com>
2019-06-16 12:53:06 -0400

    updated sunday 6/16

--- index.js
+++ index.js
@@ -35,42 +35,55 @@ function enterState(newState) {
 
 start();
 
-async function start() {
+async function start(){
   enterState("roomOne");
   let humanResponse = await ask(`182 Main St. You are standing on Main Street between 
-Church and South Winooski. There is a door here. A keypad sits on the handle.
-On the door is a handwritten sign.\n`);
+    Church and South Winooski. There is a door here. A keypad sits on the handle.
+    On the door is a handwritten sign.\n`);
   while (currentState === "roomOne") {
+      function anyNum(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
     if (humanResponse.toLowerCase() === "read sign") {
       humanResponse = await ask(`The sign says "Welcome to Burlington Code Academy! Come on up to
       the third floor. If the door is locked, use the code 12345"\n`);
     }
-    if (humanResponse.toLowerCase() === "take sign") {
-      humanResponse = await ask("That would be selfish. How will other students find their way?\n"
-      );
+    else if (humanResponse.toLowerCase() === "take sign") {
+      humanResponse = await ask("That would be selfish. How will other students find their way?\n");
     }
-    if (humanResponse.toLowerCase() === "open door") {
-      humanResponse = await ask("The door is locked. There is a keypad on the door handle.\n"
-      );
+    else if (humanResponse.toLowerCase() === "open door") {
+      humanResponse = await ask("The door is locked. There is a keypad on the door handle.\n");
     }
-    if (humanResponse.toLowerCase() === "enter code 12345" && "key in 12345") {
-      enterState("roomTwo");
-      humanResponse = await ask(`Success! The door opens. You enter the foyer and the door shuts behind you.\n`
-      );
-      /*} else if(humanResponse.toLowerCase() === 'enter code 54321') {  
-        humanResponse = await ask('Bzzzzt! The door is still locked.'); */
-       } else {
-      humanResponse = await ask("Bzzzzt! The door is still locked.\n");
+    else if (humanResponse.toLowerCase() === 'enter code 54321') {  
+    humanResponse = await ask('Bzzzzt! The door is still locked.\n'); 
+    } 
+    else if (humanResponse.toLowerCase() === 'enter code 12345') {
+      enterState('roomTwo');
+    humanResponse = await ask(`Success! The door opens. You enter the foyer and the door shuts behind you.\n`);
+    } 
+    else if (humanResponse.toLowerCase() === 'key in 12345'){
+      enterState('roomTwo');
+    humanResponse = await ask(`Success! The door opens. You enter the foyer and the door shuts behind you.\n`);
     }
+    else if (humanResponse.toLowerCase() === 'gargle') {
+    humanResponse = await ask("Sorry, I don't know how to gargle.\n")
+    }
+    else if (humanResponse.toLowerCase() === ('enter code' + anyNum(999999))) {
+    humanResponse = await ask('Bzzzzt! The door is still locked.\n')
+    } else { 
+    humanResponse = await ask('evaluate your surroundings!\n');
+    }  
   }
-  }
+  
+  
+  
 
 
-/*while (currentState === 'roomTwo') {
+while (currentState === 'roomTwo') {
   if(humanResponse === 'foyer') {
-    console.log('we made it');
-
+  humanResponse === await ask('we made it')
   }
+}
 }
 
   
